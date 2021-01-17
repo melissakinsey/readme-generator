d971acd503a97514e7876b2b75f5c138e4668751
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-01-17 16:49:47 -0600

    feat: Add TOC. Add header image. Add default responses for a few questions.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
 inquirer.prompt([
+		
 		{
 			type: "input",
-			name: "Introduction",
-			message: "What is the goal of your project?",
+			name: "Installation",
+			message: "Please give instructions for installing this app.",
 		},
 		{
 			type: "input",
 			name: "Project_Repo",
 			message: "What is the URL of the project repo?",
+			default: "[Link](https://github.com/melissakinsey/loremipsum)",
 		},
 		{
 			type: "input",
-			name: "GitHub Pages [Link](https://melissakinsey.github.io/loremipsum)",
+			name: "GitHub Pages",
 			message: "What is the URL of the project in GitHub Pages?",
+			default: "[Link](https://melissakinsey.github.io/loremipsum)",
 		},
 		{
 			type: "input",
@@ -24,12 +28,18 @@ inquirer.prompt([
 		{
 			type: "input",
 			name: "User_Story",
-			message: "What is your user story (for example, *AS A* [type of user] *I WANT TO* [perform this task] *SO I CAN* [accomplish this goal])?",
+			message: "What is your user story?",
+			default: "*AS A* [type of user] *I WANT TO* [perform this task] *SO I CAN* [accomplish this goal])."
 		},
 		{
 			type: "input",
 			name: "Description",
 			message: "How would you describe your project?",
+		},
+		{
+			type: "input",
+			name: "Usage",
+			message: "How is this app used?",
 		},
 		{
 			type: "input",
@@ -43,65 +53,104 @@ inquirer.prompt([
 		},
 		{
 			type: "input",
+			name: "Tests",
+			message: "What tests did you run?",
+		},
+		{
+			type: "input",
 			name: "Contributors",
 			message: "Was this a group project (if so, list names of contributors)?",
+			default: "This was an individual project."
 		},
 		{
 			type: "input",
 			name: "Lessons_Learned",
 			message: "What did you learn from building this app?",
 		},
+		
+	
 		{
 			type: "input",
 			name: "Image_Credits",
 			message: "Did you use any images? If so, list credits.",
 		},
 		{
-			type: "input",
-			name: "Licenses",
-			message: "List software and font licenses here.",
+			type: "list",
+			message: "Select a license for your project.",
+			choices: ["Mozilla", "Apache", "MIT"],
+			name: "License",
 		},
+		{
+		type: "input",
+		name: "Fonts",
+		message: "List software and font licenses here.",
+		default: "The {font} used under a Creative Commons license, courtesy of {designer} and Google LLC, organized under the laws of the State of Delaware, USA, and operating under the laws of the USA.",
+		},	
 	])
 	.then(function (response) {
 		console.log(response)
 		data = `
-		# INTRODUCTION
-		${response["Introduction"]}
+
+![Binary code](/code-header.png)
+
+* [Installation](#Installation)
+* [User Story](#User_Story)
+* [Description](#Description)
+* [Usage](#Usage)
+* [Features](#Features)
+* [Technical Specs](#Technical_Specs)
+* [Tests](#Tests)
+* [Contributors](#Contributors)
+* [Lessons_Learned](#Lessons_Learned)
+* [Questions](#Questions)
+
+# INSTALLATION
+${response["Installation"]}
 				
-		## Project Repo [Link](https://github.com/melissakinsey/loremipsum) 
-		${response["Project_Repo"]}
+## Project Repo 
+${response["Project_Repo"]}
 		
-		## GitHub Pages [Link](https://melissakinsey.github.io/loremipsum) 
-		${response["GitHub_Pages"]}
+## GitHub Pages 
+${response["GitHub_Pages"]}
 
-		## Screencast [Link](https://loremipsum)
-		${response["Screencast"]}
+## Screencast [Link](https://loremipsum)
+${response["Screencast"]}
 
-		# USER STORY
-		${response["User_Story"]}
+# USER STORY
+${response["User_Story"]}
 
-		# DESCRIPTION
-		${response["Description"]}
+# DESCRIPTION
+${response["Description"]}
 
-		# FEATURES
-		${response["Features"]}
+# USAGE
+${response["Usage"]}
 
-		# TECHNICAL SPECS
-		${response["Technical Specs"]}
+# FEATURES
+${response["Features"]}
 
-		# CONTRIBUTORS
-		${response["Contributors"]}
+# TECHNICAL SPECS
+${response["Technical Specs"]}
+
+# TESTS
+${response["Features"]}
+
+# CONTRIBUTORS
+${response["Contributors"]}
 			
-		# LESSONS LEARNED 
-		${response["Lessons_Learned"]}
+# LESSONS LEARNED 
+${response["Lessons_Learned"]}
 
-		# IMAGE CREDITS
-		${response["Image_Credits"]}
+# CREDITS
+## Image Credits
+${response["Image_Credits"]}
 
-		# LICENSES
-		${response["Licenses"]}
+## License
+${response["License"]}
+
+## Fonts
+${response["Fonts"]}
 		
-		`;
+`;
 
 		fs.writeFile("README.md", data, function (error) {
 			if (error) {
